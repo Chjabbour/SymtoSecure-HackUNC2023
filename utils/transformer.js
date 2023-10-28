095269a9e52dcbafe234e1cec3cea141e974ca69
vineeth dasi <vineethdasi1999@gmail.com>
2023-10-28 17:07:07 -0400

    first commit

--- utils/transformer.js
+++ utils/transformer.js
@@ -1,7 +1,25 @@
-import { encode, decode } from "js-base64";
+  import { encode, decode } from "js-base64";
+  import OpenAI from "openai";
 
-export const encodeData = (obj) => {
-  return encode(JSON.stringify(obj));
-};
+  const openai = new OpenAI({
+    apiKey: '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', 
+    dangerouslyAllowBrowser: true
+  });
 
-export const decodeData = (base64) => JSON.parse(decode(base64));
+
+  async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: 'Say this is a test' }],
+      model: 'gpt-3.5-turbo',
+    });
+  
+    console.log(chatCompletion.choices);
+  }
+  
+
+  export const encodeData = (obj) => {
+    main();
+    return encode(JSON.stringify(obj));
+  };
+
+  export const decodeData = (base64) => JSON.parse(decode(base64));
