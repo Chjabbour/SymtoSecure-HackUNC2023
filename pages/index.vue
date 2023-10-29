2a1f04abda43863fc35ab2f1eafbd1a7976d85c2
Chjabbour <68674002+Chjabbour@users.noreply.github.com>
2023-10-29 01:44:18 -0400

    remove dummy data button

--- pages/index.vue
+++ pages/index.vue
@@ -31,13 +31,13 @@
 
       </div>
       <div class="border-t bg-white flex items-center">
-        <button
+        <!-- <button
           @click="prefillDemoData"
           class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
         >
           <span> Add demo data </span>
           <icon name="mdi:code-json" class="h-4 w-4" />
-        </button>
+        </button> -->
         <button
           @click="publish"
           class="h-12 flex items-center space-x-2 px-4 border-r text-xs font-medium bg-white text-slate-700"
