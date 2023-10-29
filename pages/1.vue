baee076089adc27005c14eb15590f624e5f4067a
Chjabbour <68674002+Chjabbour@users.noreply.github.com>
2023-10-29 01:05:20 -0400

    working button analyze

--- pages/1.vue
+++ pages/1.vue
@@ -7,18 +7,49 @@
     >
       <base-loading class="h-5 w-5" />
     </div>
+    
+    <!-- Button for user to trigger analysis -->
+    <button v-if="decodedData && decodedData.d" @click="() => handleAnalysisResult(decodedData.d)">
+      Analyze Symptoms
+    </button>
 
+    <!-- Display the analysis result -->
+    <div v-if="analysisResult">
+      <h2>Analysis Result</h2>
+      <p>{{ analysisResult }}</p>
+    </div>
   </div>
 </template>
+
 <script setup>
-import { decodeData,analyzeSymptoms } from "../utils/transformer";
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { decodeData, analyzeSymptoms } from "../utils/transformer";
+
 const route = useRoute();
 const acc = route.query.data;
-const decodedData = ref({});
-decodedData.value = decodeData(acc);
-console.log(decodedData.value.d);
-analyzeSymptoms(decodedData.value.d);
+const decodedData = ref(decodeData(acc));  // Initialize decodedData with decodeData(acc) directly
+const analysisResult = ref('');
+
+const handleAnalysisResult = async (description) => {
+  const result = await analyzeSymptoms(description);
+  if (result && result.choices && result.choices[0] && result.choices[0].message) {
+    analysisResult.value = result.choices[0].message.content;
+  } else {
+    console.error('Unexpected response format:', result);
+  }
+};
+
+// If you want to call handleAnalysisResult automatically when the component mounts,
+// uncomment the following block of code:
+/*
+onMounted(() => {
+  if (decodedData.value && decodedData.value.d) {
+    handleAnalysisResult(decodedData.value.d);
+  }
+});
+*/
+
 </script>
-<script> 
-</script>
+
 <style scoped></style>
