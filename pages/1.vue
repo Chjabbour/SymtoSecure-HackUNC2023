58ce48293b671782d346584a5477516c3e6d6511
Chjabbour <68674002+Chjabbour@users.noreply.github.com>
2023-10-29 01:18:01 -0400

    working button

--- pages/1.vue
+++ pages/1.vue
@@ -22,34 +22,25 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import { ref } from 'vue';
 import { decodeData, analyzeSymptoms } from "../utils/transformer";
-
 const route = useRoute();
 const acc = route.query.data;
-const decodedData = ref(decodeData(acc));  // Initialize decodedData with decodeData(acc) directly
+const decodedData = ref({});
 const analysisResult = ref('');
+
+decodedData.value = decodeData(acc);
+console.log(decodedData.value.d);
 
 const handleAnalysisResult = async (description) => {
   const result = await analyzeSymptoms(description);
-  if (result && result.choices && result.choices[0] && result.choices[0].message) {
-    analysisResult.value = result.choices[0].message.content;
+  console.log('Result from analyzeSymptoms:', result);  // keep this line for debugging
+  if (typeof result === 'string') {
+    analysisResult.value = result;
   } else {
     console.error('Unexpected response format:', result);
   }
 };
-
-// If you want to call handleAnalysisResult automatically when the component mounts,
-// uncomment the following block of code:
-/*
-onMounted(() => {
-  if (decodedData.value && decodedData.value.d) {
-    handleAnalysisResult(decodedData.value.d);
-  }
-});
-*/
-
 </script>
 
 <style scoped></style>
